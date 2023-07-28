c20c629f94e46255c61b2a230fad8fd3918c7a2f
Dennisic\Chen <cgyforwork@foxmail.com>
2023-07-28 15:43:44 +0800

    feat: update image

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home-page">
-    <img src="./assets/header.png" class="header"/>
+    <!-- <img src="./assets/header.png" class="header"/>
     <div class="text">Network the parallel</div>
-    <div class="text">zk-based chain worlds</div>
+    <div class="text">zk-based chain worlds</div> -->
   </div>
 </template>
 
@@ -17,17 +17,17 @@
   left: 0;
   width: 100%;
   height: 100%;
-  background: url("./assets/ZK-1.gif") no-repeat center #000000;
+  background: url("./assets/ZK-2.gif") no-repeat center #000000;
   background-size: contain;
   text-align: center;
 }
 .header{
   margin-top: 30px;
   margin-bottom: 30px;
-  height: 50px;
+  height: 5vw;
 }
 .text{
-  font-size: 50px;
+  font-size: 4vw;
   font-weight: bold;
   /* color: #FFFFFF; */
   line-height: 82px;
